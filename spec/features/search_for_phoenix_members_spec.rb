47104c3de2b9e2d2953e54b38b7603640fd78c50
Andrew Reid <areid.sj@gmail.com>
2020-05-19 09:53:41 -0600

    Write and pass test for routing to search page

--- spec/features/search_for_phoenix_members_spec.rb
+++ spec/features/search_for_phoenix_members_spec.rb
@@ -1,6 +1,8 @@
-RSpec.describe 'As a user when I ' do
+require 'rails_helper'
+
+RSpec.describe "As a user when I- ", type: :feature do
   it 'visit the home page I can navigate to the search page' do
-    visit '/'
+    visit "/"
     click_on 'Search For Members'
 
     expect(current_path).to eq('/search')
